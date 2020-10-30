2975e8fb1a8d0f49727dffa2ec9bf124b3224b71
Bernard Riemann <bernard@arlin.localdomain>
2020-10-30 21:45:48 +0100

    one day of rust

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,100 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
-//struct Chip8State {
-//    n: usize,    // main address regiter ("pointer"). memsize in chip-8 are 16 bit, but we use usize
-//    i: usize, // additional 16-bit address register
-//    v: [u8; 16] // variables v0 -- vF
-//}
+// graphics part
+use pixel_canvas::{Canvas, Color, input::MouseState, image::Image};
+
+fn block_draw(im: &mut Image, x0: u8, y0: u8, w: u8, h: u8) {
+    let wd = im.width() as usize;
+    let ys = y0 as usize;
+    let xs = x0 as usize;
+    for (y, row) in im.chunks_mut(wd).enumerate() {
+        if y >= ys && y < ys+(h as usize) {
+            for (x, pxl) in row.iter_mut().enumerate() {
+                if x >= xs && x < xs+(w as usize) {
+                    *pxl = Color { r: 0, g: 0, b: 255 };
+                }
+            }
+        }
+    }
+}
 //
-//impl Chip {
-//    fn run_opcode(opcode: u16) {
-//
-//    }
-//}
 
-fn main() -> io::Result<()> {
-    println!("Hello, world!");
+struct Chip8State {
+    // ToDo: change usize to u16 for n and i
+    i: u16, // additional 16-bit address register
+    v: [u8; 16] // variables v0 -- vF
+}
 
-    let mut file = File::open("roms/tetris.rom")?;
 
-    let mut memo: [u8; 4096] = [0; 4096];
-    
-    //let bu = file.read(&mut memo)?; // maybe we need an offset here!, 0x200?
-    let bu = file.read(&mut memo[0x200..])?; // maybe we need an offset here!, 0x200?
+fn get_opcode(first: u8, second: u8) -> u16 {
+    u16::from_be_bytes([first,second])
+}
 
-    println!("{}\n", bu);
+fn get_0x00(opcode: u16) -> usize {
+    usize::from((opcode & 0x0f00) >> 8)
+}
 
-    let mut n: usize = 0x200;
+fn get_00nn(opcode: u16) -> u8 {
+   (opcode & 0x00ff) as u8
+}
 
-    let mut i: usize = 0; // additional 16-bit address register
-    let mut v: [u8; 16] = [0; 16]; // variables v0 -- vF
+impl Chip8State {
+    fn init() -> Chip8State {
+        Chip8State { i: 0, v: [0; 16] }
+    }
 
-    let mut count = 0;
-
-    loop
-    {
-        let opcode = u16::from_be_bytes([memo[n],memo[n+1]]);
-
-        //println!( "{}: {x:03} {:#04x}, next opcode {opc:#06x}", n, x=memo[n], opc=opcode);     
-
-        println!{"n {0:#05x}:", n}
-        let nold = n;
+    fn run_address(&mut self, memo: &[u8; 4096], address: u16) -> u16 {
+        let uaddr = usize::from(address);
+        let opcode = get_opcode(memo[uaddr], memo[uaddr+1]);
 
         match opcode & 0xf000 {
             0x0000 => match opcode {
                          0x00e0 => println!("{:#06x} clear screen", opcode),
                          0x00ee => println!("{:#06x} return", opcode),
-                         _ => println!("{:#06x} call machine routine {:#05x}", opcode, opcode & 0x0fff),
+                         _ => panic!("{:#06x} call RCA 1802 routine {:#05x}: not implemented", opcode, opcode & 0x0fff),
                       },
-            0x1000 => { n = usize::from(opcode & 0x0fff); println!("{:#06x} goto n={:#05x}", opcode, n); },
+            0x1000 => { 
+                         let next_addr = opcode & 0x0fff;
+                         println!("{:#06x} goto address {:#05x}", opcode, next_addr);
+                         return next_addr;
+                      },
+            0x2000 => { 
+                         let nother = opcode & 0x0fff;
+                         println!("{:#06x} run subroutine at {:#05x}", opcode, nother);
+                         if self.run_address(memo, nother) != nother+2 {
+                             panic!("no recursion support.");
+                         }
+                      },
+            0x3000 => { 
+                         let varnum = get_0x00(opcode);
+                         let num = get_00nn(opcode);
+                         println!("{:#06x} if (v{:#03x} == {:#x}) skip next", opcode, varnum, num);
+                         if self.v[varnum] == num { 
+                             return address+4;
+                         }
+                      },
+            0x4000 => { 
+                         let varnum = get_0x00(opcode);
+                         let num = get_00nn(opcode);
+                         println!("{:#06x} if (v{:#03x} != {:#x}) skip next", opcode, varnum, num);
+                         if self.v[varnum] != num { 
+                             return address+4;
+                         }
+                      },
+
+            0x6000 => { 
+                         let varnum = get_0x00(opcode);
+                         let num = get_00nn(opcode);
+                         println!("{:#06x} v{:#03x} = {:#x}", opcode, varnum, num);
+                         self.v[varnum] = num;
+                      },
+            0x7000 => { 
+                         let varnum = get_0x00(opcode);
+                         let num = get_00nn(opcode);
+                         println!("{:#06x} v{:#03x} += {:#x}", opcode, varnum, num);
+                         self.v[varnum] += num;
+                      },
             0x8000 => match opcode & 0xf00f {
                          0x8000 => println!("{:#06x} variable assignment", opcode),
                          0x8001 => println!("{:#06x} bitwise |", opcode),
@@ -61,36 +108,66 @@ fn main() -> io::Result<()> {
                          0x800e => println!("{:#06x} Vx<<=1", opcode),
                          _ => panic!("{:06x} unknown opcode!", opcode),
                       },
-            0xa000 => { i = usize::from(opcode & 0x0fff); println!("{:#06x} i = {:#05x}", opcode, i); },
+            0xa000 => { self.i = opcode & 0x0fff; println!("{:#06x} i = {:#05x}", opcode, self.i); },
+            0xd000 => { println!("{:#06x} DRAW", opcode); },
             0xf000 => match opcode & 0xf0ff {
                          0xf01e => {
-                           let varnum = usize::from((opcode & 0x0f00)>> 2);
-                           i += usize::from(v[varnum]);
-                           println!("{:#06x} i += v{:#03x} variable assignment", opcode, varnum)
+                           let varnum = get_0x00(opcode);
+                           self.i += u16::from(self.v[varnum]);
+                           println!("{:#06x} i += v{:#03x}", opcode, varnum)
                          },
                          _ => println!("{:#06x} not implemented", opcode),
                       },
-
-            _ => println!("{:#06x} not implemented", opcode),
+            _ => panic!("{:#06x} not implemented", opcode),
         }
 
-        //match memo[n] {
-        //    0x25 => println!("this is hex code 0x25"),
-        //    _    => (),
-        //}
+        return address+2
+    }
 
-        if n==nold {n += 2;}
+
+}
+
+fn main() -> io::Result<()> {
+    let filename = "roms/tetris.rom";
+
+    let mut file = File::open(filename)?;
+
+    let mut memo: [u8; 4096] = [0; 4096];
+    
+    //let bu = file.read(&mut memo)?; // maybe we need an offset here!, 0x200?
+    file.read(&mut memo[0x200..])?; // maybe we need an offset here!, 0x200?
+
+    let mut chip = Chip8State::init();
+
+    let mut count = 0;
+    let mut address: u16 = 0x200;
+    loop
+    {
+        println!("  instr {} at address {:#03x}", count, address);
+        let addr_new = chip.run_address(&memo, address);
+        if addr_new==0x1000 {
+            address += 2;  // standard flow
+        } else {
+            address = addr_new;
+        }
 
         count += 1;
-        if count > 20
+        if count > 50
         {
             break;
         }
     }
-    //for element in memo.iter() {
-    //    println!( "{}", element);
-    //}
-    v[2] = 7;
+
+    let canvas = Canvas::new(512, 512)
+        .title(filename)
+        .state(MouseState::new());
+        //.input(MouseState::handle_input);
+
+    canvas.render ( |mouse, image| {
+
+      block_draw(image, 50+0*(mouse.x as u8), 80,30,40);
+    } );
+
     Ok(())
 }
 
